67a6125658886c8695422e4deb6183f5cdbabc19
dzid26 <dzidmail@gmail.com>
2024-02-05 19:37:35 +0000

    Add traceback print

--- __init__.py
+++ __init__.py
@@ -1,2 +1,12 @@
-from .JLC2KiCad_plugin import JLC2KiCad_GUI
-JLC2KiCad_GUI().register()
+from __future__ import print_function
+import pprint
+import traceback
+import sys
+
+try:
+	print("Starting kidiff")
+	from .JLC2KiCad_plugin import JLC2KiCad_GUI
+	JLC2KiCad_GUI().register()
+except Exception as e:
+	traceback.print_exc(file=sys.stdout)
+	pprint.pprint(e)
